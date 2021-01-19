7b746ba1bdddad8ff0660ea6619147784e285b2c
heldercepeda <helder_cepeda@hotmail.com>
2021-01-19 11:27:21 +0000

    get all markets and get all pairs updated to use new table

--- ptCryptoClub/admin/gen_functions.py
+++ ptCryptoClub/admin/gen_functions.py
@@ -2,28 +2,67 @@ from ptCryptoClub.admin.config import CryptoData
 from ptCryptoClub.admin.models import LivePairs
 from ptCryptoClub.admin.sql.card_functions import last_price, price_change, max_min, volume
 from ptCryptoClub.admin.sql.latest_transactions import table_latest_trans
+from ptCryptoClub.admin.models import ErrorLogs
+from ptCryptoClub import db
 
 from sqlalchemy import create_engine
+import pandas as pd
 
 engine_live_data = create_engine(CryptoData.string)
 
 
 def get_all_markets():
+    query = """
+    select distinct(market) as market
+	    from public.live_pairs lp
+    """
+    try:
+        data = pd.read_sql_query(sql=query, con=engine_live_data)
+    except Exception as e:
+        # noinspection PyArgumentList
+        error_log = ErrorLogs(
+            route='generic functions get all markets',
+            log=str(e).replace("'", "")
+        )
+        db.session.add(error_log)
+        db.session.commit()
+        data = pd.DataFrame(columns=["market"])
     all_markets = []
-    query = LivePairs.query.with_entities(LivePairs.market).distinct()
-    for result in query:
-        all_markets.append(result.market)
+    for market in data.market:
+        all_markets.append(market)
     return all_markets
 
 
 def get_all_pairs(market):
+    query = f"""
+    select 	distinct(base) as base,
+            "quote",
+            market
+        from public.live_pairs lp
+        where quote in (
+            select distinct("quote") as pair
+                from public.live_pairs lp
+                where market = '{market}'
+        )
+        and in_use is true
+    """
+    try:
+        data = pd.read_sql_query(sql=query, con=engine_live_data)
+    except Exception as e:
+        # noinspection PyArgumentList
+        error_log = ErrorLogs(
+            route=f'generic functions get all pairs for {market}',
+            log=str(e).replace("'", "")
+        )
+        db.session.add(error_log)
+        db.session.commit()
+        data = pd.DataFrame(columns=["base", "quote", "market"])
     all_pairs = []
-    query = LivePairs.query.filter_by(market=market, in_use=True).with_entities(LivePairs.base, LivePairs.quote).distinct()
-    for result in query:
+    for i in data.index:
         all_pairs.append(
             {
-                'base': result[0],
-                'quote': result[1],
+                'base': data.base[i],
+                'quote': data.quote[i],
                 'market': market
             }
         )
