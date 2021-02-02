82c857ed89dcf59fad5aab2563bad3178b9a66d1
heldercepeda <helder_cepeda@hotmail.com>
2021-02-02 14:33:43 +0000

    icons added

---
+++ ptCryptoClub/static/js/ohlc_port.js
@@ -0,0 +1,134 @@
+
+function numberFormat(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+
+function ohlc_chart(chartDiv, market, base, quote, datapoints, candle, candle_rate) {
+
+    // Themes begin
+    am4core.useTheme(am4themes_dark);
+    // am4core.useTheme(am4themes_animated);
+    // Themes end
+
+    var chart = am4core.create(chartDiv, am4charts.XYChart);
+    chart.padding(15, 15, 15, 15);
+
+    chart.dateFormatter.inputDateFormat = "yyyy-MM-dd HH:mm:ss";
+    chart.leftAxesContainer.layout = "vertical";
+
+    chart.dataSource.url = '/api/charts/ohlc/' + market + '/' + base + '/' + quote + '/' + datapoints + '/' + candle + '/';
+    chart.dataSource.load();
+    chart.dataSource.keepCount = true;
+    chart.dataSource.parser = new am4core.JSONParser();
+    chart.dataSource.updateCurrentData = true;
+    chart.dataSource.reloadFrequency = candle_rate * 1000;
+
+    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+    dateAxis.renderer.ticks.template.length = 8;
+    dateAxis.renderer.ticks.template.strokeOpacity = 0.1;
+    dateAxis.renderer.grid.template.disabled = true;
+    dateAxis.renderer.ticks.template.disabled = false;
+    dateAxis.renderer.ticks.template.strokeOpacity = 0.2;
+    dateAxis.renderer.minLabelPosition = 0.01;
+    dateAxis.renderer.maxLabelPosition = 0.99;
+    dateAxis.keepSelection = true;
+    dateAxis.minHeight = 30;
+
+    dateAxis.start = 0.60;
+    dateAxis.end = 1;
+    dateAxis.keepSelection = true;
+
+
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.zIndex = 1;
+    valueAxis.renderer.baseGrid.disabled = true;
+    // height of axis
+    valueAxis.height = am4core.percent(90);
+
+    valueAxis.renderer.gridContainer.background.fill = am4core.color("#000000");
+    valueAxis.renderer.gridContainer.background.fillOpacity = 0.05;
+    valueAxis.renderer.inside = true;
+    valueAxis.renderer.labels.template.verticalCenter = "bottom";
+    valueAxis.renderer.labels.template.padding(2, 2, 2, 2);
+
+    valueAxis.renderer.maxLabelPosition = 0.95;
+    valueAxis.renderer.fontSize = "0.8em";
+
+    var series = chart.series.push(new am4charts.CandlestickSeries());
+    series.dataFields.dateX = "closetime";
+    series.dataFields.valueY = "closeprice";
+    series.dataFields.openValueY = "openprice";
+    series.dataFields.lowValueY = "lowprice";
+    series.dataFields.highValueY = "highprice";
+    series.tooltipText = "Open:{openValueY.value}\nHigh:{highValueY.value}\nLow:{lowValueY.value}\nClose:{valueY.value}";
+
+    // important!
+    // candlestick series colors are set in states.
+    series.riseFromOpenState.properties.fill = am4core.color("#00ff00");
+    series.dropFromOpenState.properties.fill = am4core.color("#FF0000");
+    series.riseFromOpenState.properties.stroke = am4core.color("#00ff00");
+    series.dropFromOpenState.properties.stroke = am4core.color("#FF0000");
+
+    series.riseFromPreviousState.properties.fillOpacity = 1;
+    series.dropFromPreviousState.properties.fillOpacity = 0;
+
+    // moving average BLUE
+
+    var series_ma = chart.series.push(new am4charts.LineSeries());
+    series_ma.dataFields.valueY = "moving_avg";
+    series_ma.dataFields.dateX = "closetime";
+    series_ma.strokeWidth = 1;
+    series_ma.stroke = am4core.color("#002aff");
+
+    /////////////
+
+    // moving average exp YELLOW
+
+    var series_ma = chart.series.push(new am4charts.LineSeries());
+    series_ma.dataFields.valueY = "moving_exp";
+    series_ma.dataFields.dateX = "closetime";
+    series_ma.strokeWidth = 1;
+    series_ma.stroke = am4core.color("#ffc400");
+
+    /////////////
+
+
+    var valueAxis2 = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis2.tooltip.disabled = true;
+    // height of axis
+    valueAxis2.height = am4core.percent(10);
+    valueAxis2.zIndex = 3;
+    // this makes gap between panels
+    // valueAxis2.marginTop = 30;
+    valueAxis2.renderer.baseGrid.disabled = true;
+    valueAxis2.renderer.inside = false;
+    valueAxis2.renderer.labels.template.verticalCenter = "bottom";
+    valueAxis2.renderer.labels.template.padding(2, 2, 2, 2);
+    valueAxis2.renderer.maxLabelPosition = 0.95;
+    valueAxis2.renderer.fontSize = "0.8em";
+
+    valueAxis2.renderer.gridContainer.background.fill = am4core.color("#000000");
+    valueAxis2.renderer.gridContainer.background.fillOpacity = 0.05;
+
+    var series2 = chart.series.push(new am4charts.ColumnSeries());
+    series2.dataFields.dateX = "closetime";
+    series2.clustered = false;
+    series2.dataFields.valueY = "volume";
+    series2.yAxis = valueAxis2;
+    series2.tooltipText = "{valueY.value}";
+    series2.fillOpacity = 0.2;
+    // volume should be summed
+    ///series2.groupFields.valueY = "sum";
+
+
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.behavior = "panX";
+    chart.cursor.maxPanOut = 0.005
+    // Create scrollbars
+    chart.scrollbarX = new am4core.Scrollbar();
+
+}
