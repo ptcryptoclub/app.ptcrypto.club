b37305497294f071bec3981f3a6b5b56b305edb3
heldercepeda <helder_cepeda@hotmail.com>
2021-06-02 12:12:16 +0100

    portfolio value recorded when user login

--- ptCryptoClub/admin/gen_functions.py
+++ ptCryptoClub/admin/gen_functions.py
@@ -920,6 +920,3 @@ def news_search(key_words: str, sources: str = "", page: int = 1, per_page: int 
         )
     return to_return
 
-
-if __name__ == "__main__":
-    print(calculate_total_value(user_id=1))
